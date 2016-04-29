66810df236892b7d6b13ff48c6b3b452ef9e62a7
ricardofbarros <ricardofbarros@hotmail.com>
2016-04-29 14:39:51 +0100

    update bin/disclosure

--- bin/disclosure.js
+++ bin/disclosure.js
@@ -1,38 +1,82 @@
 #!/usr/bin/env node
 
-var moduleRank = require('module-rank')
 var archy = require('archy')
 var chalk = require('chalk')
-var getProjectDeps = require('../lib/get-deps')
+var Nested = require('../lib/nested')
+var exec = require('child_process').exec
 var async = require('async')
+var charm = require('charm')()
+var path = require('path')
+var pkg = require('../package.json')
+var program = require('commander')
+var ora = require('ora')
 
-getProjectDeps(function (err, deps) {
-  if (err) {
-    return console.error(err)
-  }
+program
+  .version(pkg.version)
+  .option('-d, --depth', 'Max display depth of the dependency tree')
+  .parse(process.argv)
 
-  var tree = {}
-  var tasks = []
+charm.pipe(process.stdout)
+charm.reset()
 
-  parseDeps(deps)
-
-  function parseDeps (depsRef) {
-    var rootDeps = Object.keys(depsRef)
-
-    rootDeps.forEach(function (dep) {
-      tree[dep] = {
-        version: deps[dep].version
-      }
-
-      if (deps[dep].dependencies) {
-        tree[dep].dependencies = parseDeps()
-      }
-
-      tasks.push(function (cb) {
-        moduleRank(dep, deps[dep].version, function () {
-
-        })
-      })
-    })
-  }
+var spinner = ora({
+  text: 'Loading dependencies',
+  spinner: 'circleHalves',
+  color: 'yellow'
 })
+
+getProjectDeps(function (err, viewObj) {
+  spinner.start()
+
+  if (err) {
+    return handleError(err)
+  }
+
+  var deps = new Nested(viewObj.name, viewObj.dependencies)
+
+  async.whilst(
+    function () {
+      return deps.next()
+    },
+    function (next) {
+      deps.process(next)
+    },
+    function (err) {
+      if (err) {
+        return handleError(err)
+      }
+
+      spinner.stop()
+      charm.write(archy(deps.archyObj))
+    }
+  )
+})
+
+function getProjectDeps (cb) {
+  var depthStr = ''
+
+  if (program.depth) {
+    depthStr = ' --depth ' + program.depth
+  }
+
+  exec('npm ls --json true --only prod' + depthStr, { cwd: process.cwd() }, function (err, stdout, stderr) {
+    if (err || stderr) {
+      return cb(err || new Error('Something wrong happened.'))
+    }
+
+    try {
+      var modl = JSON.parse(stdout)
+
+      return cb(null, modl)
+    } catch (e) {
+      cb(e)
+    }
+  })
+}
+
+function handleError (err) {
+  spinner.stop()
+  return charm
+    .write(err.stack)
+    .write('\n')
+}
