52dabeaa23ca5eb84afd5d31ffc6cbb346de44d3
ricardofbarros <ricardofbarros@hotmail.com>
2016-05-19 17:30:37 +0100

    add reporter option

--- bin/disclosure.js
+++ bin/disclosure.js
@@ -1,43 +1,34 @@
 #!/usr/bin/env node
 
 var async = require('async')
-var charm = require('charm')()
 var pkg = require('../package.json')
 var program = require('commander')
-var ora = require('ora')
 var localData = require('module-data').local
 var remoteData = require('module-data').remote
 var standardizeData = require('module-data/standardize')
 var getDepsSet = require('module-data/dependencies-set')
 var moduleRank = require('module-rank')
-var Table = require('cli-table2')
-var chalk = require('chalk')
 var path = require('path')
+var tableReporter = require('../lib/table-reporter')
 
+var reporters = {
+  'table': tableReporter,
+  'default': defaultDisplay
+}
 var pathArg
 
 // Parse args path
 // Describe program
 program
   .version(pkg.version)
+  .option('--reporter <reporter>', 'Beatify data with a reporter', selectReporter, 'default')
+  .option('--licenses [licenses]', 'Licences whitelist separated by commas')
+  .option('--licenses-file [licensesFile]', 'Licences whitelist json')
   .arguments('<path>')
   .action(function (path) {
     pathArg = path
   })
   .parse(process.argv)
-
-// Handle stdout and clean terminal
-charm.pipe(process.stdout)
-charm.reset()
-
-// Create loading spinner
-var spinner = ora({
-  text: 'Loading dependencies',
-  spinner: 'circleHalves',
-  color: 'yellow'
-})
-
-spinner.start()
 
 localData(path.resolve(pathArg), {depth: 0}, function (err, local) {
   if (err) {
@@ -54,10 +45,21 @@ localData(path.resolve(pathArg), {depth: 0}, function (err, local) {
   return async.parallel(queries, handleRemoteData(local))
 })
 
+function selectReporter (val, def) {
+  if (!val) {
+    val = def
+  }
+
+  if (!~Object.keys(reporters).indexOf(val)) {
+    handleError(new Error(val + ' not a valid reporter.'))
+    return null
+  }
+
+  return val
+}
+
 function handleRemoteData (local) {
   return function (err, remote) {
-    spinner.stop()
-
     if (err) {
       return handleError(err)
     }
@@ -72,9 +74,9 @@ function handleRemoteData (local) {
 }
 
 function handleError (err) {
-  charm
-    .write(err.stack)
-    .write('\n')
+  console.log(err.stack)
+
+  process.exit(1)
 }
 
 function mergeData (data, done) {
@@ -92,67 +94,11 @@ function displayData (err, data) {
     return handleError(err)
   }
 
-  var table = new Table({
-    head: [
-      chalk.cyan('Module name'),
-      chalk.cyan('License'),
-      chalk.cyan('Reliability'),
-      chalk.cyan('Security'),
-      chalk.cyan('SLOC (Weight)'),
-      chalk.cyan('Score')
-    ]
-  })
-
-  var depsKeys = Object.keys(data)
-  var totalSloc = 0
-
-  depsKeys.forEach(function (dep) {
-    totalSloc += data[dep].sloc.real
-  })
-
-  depsKeys.forEach(function (dep) {
-    var crit = data[dep].criteria
-    var sloc = data[dep].sloc.real
-
-    table.push([
-      [dep, data[dep].version].join('@'),
-      colorLicense(data[dep].license, crit.license.score),
-      colorPercentage(crit.reliability.score),
-      colorPercentage(crit.security.score),
-      sloc + ' (' + parseFloat(((sloc * 100) / totalSloc).toFixed(2)) + ' %)',
-      colorPercentage(data[dep].score)
-    ])
-  })
-
-  charm
-    .write(table.toString())
-    .write('\n')
+  console.log(reporters[program.reporter](data))
 }
 
-function colorLicense (license, licenseScore) {
-  if (!licenseScore) {
-    return chalk.red(license)
-  }
-
-  return license
-}
-
-function colorPercentage (num) {
-  num *= 100
-
-  if (num >= 0 && num <= 33) {
-    num = chalk.red(num + ' %')
-  }
-
-  if (num > 33 && num <= 66) {
-    num = chalk.yellow(num + ' %')
-  }
-
-  if (num > 66) {
-    num = chalk.green(num + ' %')
-  }
-
-  return num
+function defaultDisplay (data) {
+  return JSON.stringify(data, null, 2)
 }
 
 function getData (mdlName, version) {
